c1c083d1e3bc88a461f4d9f16acd6011e0245d80
vividwhisp <korivinesh@gmail.com>
2026-05-20 18:25:30 +0530

    logic and handle click event created

--- react-next-js/my-app/src/App.js
+++ react-next-js/my-app/src/App.js
@@ -1,20 +1,43 @@
-export default function Square() {
+import './App.css';
+import { useState } from 'react';
+
+
+function Square() {
+  const [value,setValue] = useState(null)
+
+  function handleClick(){
+    setValue("X")
+  }
   return (
-    <><div className="board-row">
-    <button className="square">X</button>
-    <button className="square">X</button>
-    <button className="square">X</button>
+  <button 
+     className="square"
+    onClick={handleClick}
+  >
+    {value}
+    </button>
+  );
+}
+
+
+
+export default function Board() {
+  return (
+    <div className="game">
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
     </div>
-    <div className="board-row">
-    <button className="square">X</button>
-    <button className="square">X</button>
-    <button className="square">X</button>
-    </div>
-    <div className="board-row">
-    <button className="square">X</button>
-    <button className="square">X</button>
-    <button className="square">X</button>
-    </div>
-    </>
   );
 }

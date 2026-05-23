4bebbd72de27ff384385a6c06aa2c660ae55e9b4
vividwhisp <korivinesh@gmail.com>
2026-05-23 18:01:57 +0530

    array, loops, slices done

--- go/array_loops.go
+++ go/array_loops.go
@@ -1,11 +1,36 @@
 package main
 
 import "fmt"
-func main(){
-	var intArr [3] int32
-	intArr[1] = 123
-	fmt.Println(intArr[0])
-	fmt.Println(intArr[0:3])
-	fmt.Println(&intArr[0])
 
+func main(){
+	 intArr := [...]int32{1,2,3}
+	fmt.Println(intArr)	
+
+	var intSlice []int32 = []int32{4,5,6}
+	fmt.Println(intSlice)
+	intSlice = append(intSlice,7)
+	fmt.Println(intSlice)
+
+	var myMap map[string]uint8 = make(map[string]uint8)
+	fmt.Println(myMap)
+
+	var myMap2  = map[string]uint8{"Raj":20,"Shiva":24}
+	fmt.Println(myMap2["Raj"])
+
+	var age, ok = myMap2["R"]
+	if ok{
+		fmt.Printf("Age is %v\n",age)
+	}else{
+		fmt.Println(ok)
+	}
+
+	for name,age := range myMap2{
+		fmt.Printf("Name: %v age: %v\n",name ,age)
+	}
+	for i, v := range intArr{
+		fmt.Printf("Index: %v, Value: %v\n",i,v)
+	}
+	for i:=0; i<=10; i++{
+		fmt.Println(i)
+	}
 }
--- go/loops_and_control.go
+++ go/loops_and_control.go
@@ -1,13 +1,6 @@
 package main
 
 import "fmt"
-
-
-func main(){
-	printRem(6, 2)
-	
-}
-
 func printRem(numerator int, denominator int) int {
 	rem := numerator / denominator
 	fmt.Println(rem)

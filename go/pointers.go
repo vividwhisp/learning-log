88a8c0900cc066fb479a22ae95a4e617273c31f4
vividwhisp <korivinesh@gmail.com>
2026-06-12 00:17:46 +0530

    slice pointer done

--- go/pointers.go
+++ go/pointers.go
@@ -1,5 +1,8 @@
 package main
-import "fmt"
+
+import (
+	"fmt"
+)
 
 func main(){
 	// var p *int32 = new(int32)
@@ -14,6 +17,24 @@ func main(){
 	// i = k
 	// fmt.Printf("the value of i is : %v", i)
 
-	var slice = []int32{1,2,3}
-	var sliceCopy = slice
+	// var slice = []int32{1,2,3}
+	// var sliceCopy = slice
+	// sliceCopy[2] = 4
+	// fmt.Println(slice) 
+	// fmt.Println(sliceCopy)
+	var thing1 =  [5]float64{1,2,3,4,5}
+	fmt.Printf("\n the memory location of thing1 array is: %p", &thing1)
+	var result [5]float64 = square(&thing1)
+	fmt.Printf("\n The result is: %v",result)
+	fmt.Printf("\n the value of thing1 is : %v", thing1)
+
+ 
+}
+
+func square(thing2 *[5]float64) [5]float64{
+	fmt.Printf("\n the memory location of thing2 is : %p", &thing2)
+	for i := range thing2{
+		thing2[i] = thing2[i] * thing2[i]
+	}
+	return *thing2
 }

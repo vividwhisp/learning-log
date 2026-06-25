0eb2371913a30c58801c5052159fbf0d0eba95f1
vividwhisp <korivinesh@gmail.com>
2026-06-25 18:13:06 +0530

    change the localhost:8000 to :8000 to build the docker image

--- go/api/cmd/api/main.go
+++ go/api/cmd/api/main.go
@@ -16,7 +16,7 @@ func main(){
 	handlers.Handler(r)
 
 	fmt.Println("starting GO API service")
-	err := http.ListenAndServe("localhost:8000",r)
+	err := http.ListenAndServe(":8000",r)
 	if err != nil{
 		log.Error(err)
 	}

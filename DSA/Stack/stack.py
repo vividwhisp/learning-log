29a49fa36441d47f4063c7df8349ff46295925f3
vividwhisp <korivinesh@gmail.com>
2026-07-31 11:32:39 +0530

    DSA: Stack: Peek, pop in stack

--- DSA/Stack/stack.py
+++ DSA/Stack/stack.py
@@ -8,4 +8,25 @@ class Stack:
     def push(self, value):
         self.s.insert(0, value)
 
-    
+    def peek(self):
+        if len(self.s) == 0:
+            raise Exception("Stack is Empty")
+        else:
+            return self.s[0]
+
+    def pop(self):
+        if len(self.s) == 0:
+            raise Exception("Stack is Empty")
+        else:
+            return self.s.pop(0)
+
+
+obj = Stack()
+obj.push(10)
+obj.push(20)
+obj.push(30)
+print(obj.pop())
+print(obj.pop())
+print(obj.pop())
+print(obj.peek())
+

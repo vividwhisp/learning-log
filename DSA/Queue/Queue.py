df4a09aef169a5557eab83e0825ad41469a43ea9
vividwhisp <korivinesh@gmail.com>
2026-08-01 18:49:45 +0530

    DSA: Queue: Deletion and testing

--- DSA/Queue/Queue.py
+++ DSA/Queue/Queue.py
@@ -8,5 +8,17 @@ class Queue:
     def insert(self,value):
         self.items.append(value)
 
-    
+    def delete(self):
+        if self.isEmpty():
+            raise Exception("Queue is Empty")
+        else:
+            return self.items.pop(0)
+
+q = Queue()
+q.insert(10)
+q.insert(20)
+q.insert(30)
+print(q.delete())
+print(q.delete())
+print(q.delete())
 

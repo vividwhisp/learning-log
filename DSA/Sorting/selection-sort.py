6ebab687a4d152fd95ab66b937f192bc3954856c
vividwhisp <korivinesh@gmail.com>
2026-08-29 23:24:59 +0530

    DSA: Sorting: Selection Sort

--- DSA/Sorting/selection-sort.py
+++ DSA/Sorting/selection-sort.py
@@ -1,2 +1,13 @@
-def selectionSort():
-    pass
+def selectionSort(a):
+    n = len(a)
+
+    for i in range(n):
+        min = i
+        for j in range(i, n):
+            if a[min] < a[j]:
+                min = j
+        a[i], a[min] = a[min], a[i]
+
+a= [64,32,25,50,45,40,51]
+selectionSort(a)
+print(a)

67024ccf51b413ec16c5b464aa85a516351e824d
vividwhisp <korivinesh@gmail.com>
2026-09-02 23:56:53 +0530

    DSA: Sorting: QuickSort:- Completed

--- DSA/Sorting/quick-sort.py
+++ DSA/Sorting/quick-sort.py
@@ -1,9 +1,35 @@
 def quickSort(arr, l, r):
     if(l<r):
-        partition(arr, l, r)
-        
+       p = partition(arr, l, r)
+       quickSort(arr, l, p-1)
+       quickSort(arr, p+1, r)
+
+    
 
 
-def partition():
-    pass
+def partition(arr, l, r):
+    pivot = arr[l]
+    i = l + 1
+    j = r
+
+    while True:
+        while i < j and arr[i] < pivot:
+            i += 1
+        while i < j and arr[j] > pivot:
+            j -= 1
+        if i < j:
+            arr[i], arr[j] = arr[j], arr[i]
+        else: 
+            break
+
+    arr[l], arr[j] = arr[j], arr[l] 
+
+    return j
+
+a = [64,32,25,50,45,40,51]
+quickSort(a, 0, len(a) - 1)
+print(a)
+
+
+    
     

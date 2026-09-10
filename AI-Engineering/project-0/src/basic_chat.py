e9cad5d3935b14e52bef3b42cb0771ba3d54e612
vividwhisp <korivinesh@gmail.com>
2026-09-10 18:16:48 +0530

    AI-Engg: Project-0: basic chat one to one done

---
+++ AI-Engineering/project-0/src/basic_chat.py
@@ -0,0 +1,24 @@
+import os
+import sys
+from dotenv import load_dotenv  # type: ignore
+from openai import OpenAI  # type: ignore
+
+load_dotenv()
+api_key = os.getenv("GROQ_API_KEY")
+
+if not api_key:
+    raise ValueError("GROQ_API_KEY is not set")
+
+client = OpenAI(api_key=api_key,
+                base_url="https://api.groq.com/openai/v1")
+while True:
+    user_input = input("You: ")
+    if user_input == "exit":
+        print("Exiting..")
+        sys.exit()
+    else:
+        response = client.responses.create(
+            model="openai/gpt-oss-20b",
+            input=user_input
+        )
+        print("AI: " + response.output_text)
